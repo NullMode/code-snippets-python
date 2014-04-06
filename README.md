fcbcfbf6c7c3b1b87fafa5c72f2304de44beed3b
NullMode <nullmode@gmail.com>
2014-04-06 11:22:56 +0100

    Adds python percentage loader

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ code-snippets-python
 ====================
 
 A collection of code snippets in python that might be useful for others.
+
+percentage_output_loading.py
+============================
+
+This snippet takes a text file, reads in each line, and whilst doing so gives a constantly updating percentage of it's progress.
---
+++ percentage_output_loading.py
@@ -0,0 +1,21 @@
+from __future__ import division
+from time import sleep
+import sys
+
+# Set input file and sleep time
+file_name = 'input.txt'
+sleep_amount = 0.05
+
+
+test_file = open(file_name, 'r').readlines()
+line_count = len(test_file)
+
+c = 1
+for i in test_file:
+	sys.stdout.write('\r')
+	value = int(round((c / line_count) * 100))
+	sys.stdout.write(str(value) + "%") 
+	sys.stdout.flush()
+	c = c + 1
+	sleep(sleep_amount)
+sys.stdout.write("\n")
