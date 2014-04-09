774424dd3b9f5baf055506a0dafc9c52b77d3e2a
NullMode <nullmode@gmail.com>
2014-04-09 18:26:44 +0100

    Adds obscure script that I couldn't write a decent description for. Examples FTW

---
+++ for_each_word_in_file_add_each_word_in_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import sys
+
+if sys.argv == 1:
+    print "./script <input file> <output file>"
+    sys.exit(1)
+
+filein = open(sys.argv[1], "r")
+fileout = open(sys.argv[2], "w")
+
+filein_list = []
+for i in filein:
+    filein_list.append(i.strip())
+filein.close()
+
+filein_list2 = filein_list
+
+for i in filein_list:
+        for j in filein_list2:
+                fileout.write(i + j)
+fileout.close()
