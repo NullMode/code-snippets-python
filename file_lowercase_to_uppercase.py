e6619f6d81b8e060b57bcb3ec176870f63aa17fa
NullMode <nullmode@gmail.com>
2014-04-13 16:15:16 +0100

    Adds to scripts for uppercase to lowercase conversion. Adds short description to README

---
+++ file_lowercase_to_uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+import sys
+
+if len(sys.argv) != 3:
+    print "[*} Usage: ./script <input file> <output file>"
+
+file1 = open(sys.argv[1], "r")
+file2 = open(sys.argv[2], "w")
+
+for i in file1:
+        file2.write(i.upper())
+        
+file1.close()
+file2.close()
